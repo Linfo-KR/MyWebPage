bfda293c8917dda670e9ca8e4dd8b3474b50c1cc
Linfo-KR <linfo4931@gmail.com>
2024-02-20 20:23:58 +0900

    Create Post Model and Set Up Timezone

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model) :
+    title = models.CharField(max_length = 30)
+    content = models.TextField()
+    createTime = models.DateTimeField(auto_now_add = True)
+    updateTime = models.DateTimeField(auto_now = True)
+    # author = None / Preparing...
+    
+    def __str__(self) :
+        return f'[{self.pk}] {self.title}'
