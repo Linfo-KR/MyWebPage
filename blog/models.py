c047ceb5eebeafc51787ccbbe112386af4658242
Linfo-KR <linfo4931@gmail.com>
2024-02-22 15:38:36 +0900

    Apply on Author Field

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 import os
 
 from django.db import models
+from django.contrib.auth.models import User
 
 class Post(models.Model) :
     title = models.CharField(max_length=30)
@@ -10,10 +11,10 @@ class Post(models.Model) :
     updateTime = models.DateTimeField(auto_now = True)
     headImage = models.ImageField(upload_to='blog/images/%Y/%m/%d', blank=True)
     fileUpload = models.FileField(upload_to='blog/files/%Y/%m/%d', blank=True)
-    # author = None / Preparing...
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title} :: {self.author}'
     
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
