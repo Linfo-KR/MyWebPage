6e170faad929269bb8d39348b6e7e5a98273854a
Linfo-KR <linfo4931@gmail.com>
2024-02-23 16:55:47 +0900

    Apply on Tag Function in PostList and PostDetail Pages

--- blog/models.py
+++ blog/models.py
@@ -16,6 +16,17 @@ class Category(models.Model):
     
     class Meta:
         verbose_name_plural = 'Categories'
+        
+        
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+    
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return f'/blog/tag/{self.slug}/'
 
 class Post(models.Model) :
     title = models.CharField(max_length=30)
@@ -27,6 +38,7 @@ class Post(models.Model) :
     fileUpload = models.FileField(upload_to='blog/files/%Y/%m/%d', blank=True)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL, blank=True)
+    tags = models.ManyToManyField(Tag, blank=True)
     
     def __str__(self):
         return f'[{self.pk}]{self.title} :: {self.author}'
--- blog/tests.py
+++ blog/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
 from django.contrib.auth.models import User
-from .models import Post, Category
+from .models import Post, Category, Tag
 
 
 class TestView(TestCase):
@@ -11,9 +11,15 @@ class TestView(TestCase):
         self.user02 = User.objects.create_user(username='linfo-us', password='linfo-us')
         self.categoryBackend = Category.objects.create(name='Backend', slug='backend')
         self.categoryDeepLearning = Category.objects.create(name='Deep Learning', slug='deep-learning')
+        self.tagDjango = Tag.objects.create(name='Django', slug='django')
+        self.tagReact = Tag.objects.create(name='React', slug='react')
+        self.tagFlutter = Tag.objects.create(name='Flutter', slug='flutter')
         self.post001 = Post.objects.create(title = 'First Post', content = 'This is First Post', author = self.user01, category=self.categoryBackend)
         self.post002 = Post.objects.create(title = 'Second Post', content = 'This is Second Post', author = self.user02, category=self.categoryDeepLearning)
         self.post003 = Post.objects.create(title = 'Third Post', content = 'This is Third Post', author = self.user02)
+        self.post001.tags.add(self.tagDjango)
+        self.post003.tags.add(self.tagReact)
+        self.post003.tags.add(self.tagFlutter)
         
     def category_card_test(self, soup):
         categoriesCard = soup.find('div', id='categories-card')
@@ -56,12 +62,23 @@ class TestView(TestCase):
         post001Card = mainArea.find('div', id="post-1")
         self.assertIn(self.post001.title, post001Card.text)
         self.assertIn(self.post001.category.name, post001Card.text)
+        self.assertIn(self.tagDjango.name, post001Card.text)
+        self.assertNotIn(self.tagReact.name, post001Card.text)
+        self.assertNotIn(self.tagFlutter.name, post001Card.text)
+        
         post002Card = mainArea.find('div', id="post-2")
         self.assertIn(self.post002.title, post002Card.text)
         self.assertIn(self.post002.category.name, post002Card.text)
+        self.assertNotIn(self.tagDjango.name, post002Card.text)
+        self.assertNotIn(self.tagReact.name, post002Card.text)
+        self.assertNotIn(self.tagFlutter.name, post002Card.text)
+        
         post003Card = mainArea.find('div', id="post-3")
         self.assertIn('Unclassified', post003Card.text)
         self.assertIn(self.post003.title, post003Card.text)
+        self.assertNotIn(self.tagDjango.name, post003Card.text)
+        self.assertIn(self.tagReact.name, post003Card.text)
+        self.assertIn(self.tagFlutter.name, post003Card.text)
         
         self.assertIn(self.user01.username.upper(), mainArea.text)
         self.assertIn(self.user02.username.upper(), mainArea.text)
@@ -75,29 +92,27 @@ class TestView(TestCase):
         self.assertIn('Any Posts not Exist', mainArea.text)
         
     def test_post_detail(self):
-        # 1.1 One Post is Exist
-        # 1.2 The Post's URL is '/blog/1/'
         self.assertEqual(self.post001.get_absolute_url(), '/blog/1/')
-        
-        # 2.0 Detail Page Test of First Post
-        # 2.1 Approaching url on the first post works fine(status code: 200)
+    
         response = self.client.get(self.post001.get_absolute_url())
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
-        # 2.2 Navigation Bar and Category Card Test
+
         self.navbar_test(soup)
         self.category_card_test(soup)
-        # 2.3 The title of the first post is in the Web browser tab title
+
         self.assertIn(self.post001.title, soup.title.text)
-        # 2.4 The title of the first post exists in the post area
+
         mainArea = soup.find('div', id="main-area")
         postArea = soup.find('div', id="post-area")
         self.assertIn(self.post001.title, postArea.text)
         self.assertIn(self.categoryBackend.name, postArea.text)
-        # 2.5 The author of the first post exists in the post area(None Development)
         self.assertIn(self.user01.username.upper(), postArea.text)
-        # 2.6 The content of the first post exists in the post area
         self.assertIn(self.post001.content, postArea.text)
+        
+        self.assertIn(self.tagDjango.name, postArea.text)
+        self.assertNotIn(self.tagReact.name, postArea.text)
+        self.assertNotIn(self.tagFlutter.name, postArea.text)
         
     def test_category_page(self):
         response = self.client.get(self.categoryBackend.get_absolute_url())
