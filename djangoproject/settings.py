55ba6c157698bce6c36ab0266762d2800d102a12
Linfo-KR <linfo4931@gmail.com>
2024-02-24 17:32:56 +0900

    Apply on Django Crispy Form

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'crispy_forms',
     'blog',
     'singlepages',
 ]
@@ -122,6 +123,7 @@ STATIC_URL = 'static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
